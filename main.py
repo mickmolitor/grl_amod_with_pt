fbdd174559dd46697d9179372d2cd2a9045389a8
Dominik Phillip Theilen <dominik.phillip.theilen@sap.com>
2024-04-26 07:34:23 +0200

    Add shortest paths skript

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from params.program_params import Mode, ProgramParams
 from playground.graph_sage_test import test_graph_sage
 from program.grid.grid import Grid
+from static_data_generation.public_transport_graph_creation import generate_shortest_paths_graph
 from static_data_generation.zone_graph_creation import create_zone_graph, fix_zone_graph
 from visualization.visualize_graph import visualize_zone_graph
 
@@ -7,8 +9,7 @@ from visualization.visualize_graph import visualize_zone_graph
 #create_zone_graph()
 #visualize_zone_graph()
 #fix_zone_graph()
-
-test_graph_sage()
+#test_graph_sage()
 
 # Orders einlesen
 # State aktualisieren
@@ -17,3 +18,144 @@ test_graph_sage()
 # State updaten
 # relocations
 # state value updaten
+
+while True:
+    user_input = input(
+        "Which menu you want to enter? (Graph Reinforcement Learning -> 1, Baseline Performance -> 2, Static Data Generation -> 3, Visualization -> 4, Data Analysis -> 5) "
+    )
+    if user_input == "1":
+        # ProgramParams.EXECUTION_MODE = Mode.GRAPH_REINFORCEMENT_LEARNING
+        # while True:
+        #     user_input = input(
+        #         "Which script do you want to start? (Online Training and Testing -> 1, Start Q-Learning (one day)-> 2) "
+        #     )
+        #     if user_input == "1":
+        #         StateValueTable.get_state_value_table().raze_state_value_table()
+        #         initialize_driver_positions()
+        #         # Train the algorithm On-Policy
+        #         for i in range(14):
+        #             start_q_learning()
+        #             Order.reset()
+        #             State.reset()
+        #             ProgramParams.SIMULATION_DATE += timedelta(1)
+        #         # Testing
+        #         Drivers.raze_drivers()
+        #         initialize_driver_positions()
+        #         # Test the algorithm On-Policy
+        #         for i in range(7):
+        #             start_q_learning()
+        #             Order.reset()
+        #             State.reset()
+        #             ProgramParams.SIMULATION_DATE += timedelta(1)
+        #         break
+        #     if user_input == "2":
+        #         start_q_learning()
+        #         break
+        #     else:
+        #         print("This option is not allowed. Please try again.")
+        break
+
+    elif user_input == "2":
+        # while True:
+        #     ProgramParams.EXECUTION_MODE = Mode.BASELINE_PERFORMANCE
+        #     user_input = input(
+        #         "Which script do you want to start? (Offline Policy Evaluation -> 1, Online Training -> 2, Start DRL (one day) -> 3) "
+        #     )
+        #     if user_input == "1":
+        #         initialize_driver_positions()
+        #         # Train the ope networks with online data
+        #         for i in range(14):
+        #             train_ope()
+        #             Order.reset()
+        #             State.reset()
+        #             ProgramParams.SIMULATION_DATE += timedelta(1)
+        #         break
+        #     elif user_input == "2":
+        #         initialize_driver_positions()
+        #         # Train the algorithm On-Policy
+        #         # You have to set the date in program_params on the Date you want (old date + train duration)
+        #         for i in range(7):
+        #             start_drl()
+        #             Order.reset()
+        #             State.reset()
+        #             ProgramParams.SIMULATION_DATE += timedelta(1)
+        #         break
+        #     elif user_input == "3":
+        #         start_drl()
+        #         break
+        #     else:
+        #         print("This option is not allowed. Please try again.")
+        break
+
+    elif user_input == "3":
+        while True:
+            user_input = input(
+                "Which script do you want to start? (Create Zone Graph -> 1, Create Public Transport Graph -> 2) "
+            )
+            if user_input == "1":
+                create_zone_graph()        
+                break
+            if user_input == "2":
+                generate_shortest_paths_graph()
+                break
+            else:
+                print("This option is not allowed. Please try again.")
+        break
+
+    elif user_input == "4":
+        # while True:
+        #     user_input = input(
+        #         "Which script do you want to start? (Grid Cell Creation -> 1, Generate Trajectories -> 2, Remove Idle Trajectories -> 3, Initialize Drivers -> 4, Discretize days -> 5) "
+        #     )
+        #     if user_input == "1":
+        #         create_cell_grid()
+        #         break
+        #     elif user_input == "2":
+        #         initialize_driver_positions_for_trajectories()
+        #         generate_trajectories()
+        #         break
+        #     elif user_input == "3":
+        #         remove_idle_trajectories()
+        #         break
+        #     elif user_input == "4":
+        #         initialize_driver_positions()
+        #         break
+        #     elif user_input == "5":
+        #         TimeSeriesDiscretization.discretize_day()
+        #         break
+        #     else:
+        #         print("This option is not allowed. Please try again.")
+        break
+
+    elif user_input == "5":
+        # while True:
+        #     user_input = input(
+        #         "Which script do you want to start? (Visualize driver positions -> 1, Visualize order positions -> 2, Visualize trip data -> 3) "
+        #     )
+        #     if user_input == "1":
+        #         visualize_drivers(f"drivers_{ProgramParams.SIMULATION_DATE.strftime('%Y-%m-%d')}.png")
+        #         break
+        #     elif user_input == "2":
+        #         visualize_orders(f"orders_{ProgramParams.SIMULATION_DATE.strftime('%Y-%m-%d')}.png")
+        #         break
+        #     elif user_input == "3":
+        #         visualize_trip_data()
+        #         break
+        #     else:
+        #         print("This option is not allowed. Please try again.")
+        break
+
+    # elif user_input == "6":
+    #     while True:
+    #         user_input = input(
+    #             "Which script do you want to start? (Analyse trip data (data_analysis.py need to be adapted[date and folder structure]) -> 1) "
+    #         )
+    #         if user_input == "1":
+    #             analyse_trip_data()
+    #             break
+    #         else:
+    #             print("This option is not allowed. Please try again.")
+    #     break
+
+    else:
+        print("This option is not allowed. Please try again.")
---
+++ static_data_generation/public_transport_graph_creation.py
@@ -0,0 +1,165 @@
+from collections import deque, namedtuple
+import csv
+import math
+from params.program_params import ProgramParams
+
+# This file generates the fastest connection public transit network only
+# This is a Dijkstra algorithm. For a faster solution use the Floyd-Warshall algorithm to solve the all-pair shortest path problem (see code/model_builder)
+Edge = namedtuple("Edge", "start, end, cost")
+
+def create_edge(start, end, cost):
+    return Edge(start, end, cost)
+
+class Graph:
+    def __init__(self, edges):
+        self.edges = [create_edge(*e) for e in edges]
+
+    def vertices(self): 
+        return set(e.start for e in self.edges).union(e.end for e in self.edges)
+    
+    def get_neighbours(self, v):
+        neighbours = []
+        for e in self.edges:
+            if e.start == v:
+                neighbours.append((e.end, e.cost))
+        return neighbours
+
+    def dijkstra(self, source, destination): 
+        distances = {v: float("inf") for v in self.vertices()} # each node gets a distance of infinity
+        prev_v = {v: None for v in self.vertices()} # each node gets a predecessor of None
+
+        distances[source] = 0 # the distance of the start node is set to 0
+        vertices = list(self.vertices())[:]
+        while len(vertices) > 0:
+            v = min(vertices, key=lambda u: distances[u]) # the node with the smallest distance is selected
+            vertices.remove(v) # the node is removed from the list
+            if distances[v] == float("inf"):
+                break # if the distance of the node is infinite, then there is no path to the destination node
+            for neighbour, cost in self.get_neighbours(v):
+                path_cost = distances[v] + cost # the distance of the neighbor is calculated
+                if path_cost < distances[neighbour]:
+                    distances[neighbour] = path_cost
+                    prev_v[neighbour] = v
+        path = []
+        curr_v = destination
+        while curr_v and prev_v[curr_v] is not None:
+            path.insert(0, curr_v)
+            curr_v = prev_v[curr_v] # the predecessor of the node becomes the current node
+        if curr_v:
+            path.insert(0, curr_v)
+        return path, distances[destination]
+
+###############################################################################
+
+def lat_lon_to_meters(lat1, lon1, lat2, lon2):
+    # Conversion factors
+    meters_per_degree_lat = 111000  # approximately 111 kilometers per degree
+    meters_per_degree_lon = meters_per_degree_lat * math.cos(math.radians((lat1 + lat2) / 2))
+
+    # Conversion to meters
+    delta_lat_meters = (lat1 - lat2) * meters_per_degree_lat
+    delta_lon_meters = (lon1 - lon2) * meters_per_degree_lon
+
+    return delta_lat_meters, delta_lon_meters
+
+def manhattan_distance(lat1, lon1, lat2, lon2):
+    delta_lat_meters, delta_lon_meters = lat_lon_to_meters(lat1, lon1, lat2, lon2)
+    return abs(delta_lat_meters) + abs(delta_lon_meters)
+
+###############################################################################
+
+def read_csv_data(filename):
+    data = []
+    with open(filename, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append((int(row["ID"]), row["station_name"], row["line"], float(row["LAT"]), float(row["LONG"])))
+    return data
+
+###############################################################################
+
+def create_edges(data):
+    edges = []
+    station_duration = ProgramParams.STATION_DURATION # Set the value for Station_Duration here
+    transfer_same_station = ProgramParams.TRANSFER_SAME_STATION  # Set the value for Transfer_Same_Station here
+    max_walking_duration = ProgramParams.MAX_WALKING_DURATION
+    walking_speed = ProgramParams.WALKING_SPEED 
+    
+    # Count variables for the different types of edges
+    line_edges_count = 0
+    transfer_edges_count = 0
+    remaining_edges_count = 0
+
+    # Helper functions and structures
+    id_to_name = {station_id: stop_name for station_id, stop_name, _ , _, _ in data}
+    name_to_ids = {}
+    for station_id, stop_name, _, _, _ in data:
+        if stop_name not in name_to_ids:
+            name_to_ids[stop_name] = []
+        name_to_ids[stop_name].append(station_id)
+
+    # Edges for the same line
+    for i in range(len(data) - 1):
+        id1, name1, line1, lat1, lon1 = data[i]
+        id2, name2, line2, lat2, lon2 = data[i + 1]
+        if line1 == line2:
+            edges.append((id1, id2, station_duration))
+            edges.append((id2, id1, station_duration))
+            line_edges_count += 1
+
+    # Edges for transfers at the same station
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            id1, name1, line1, lat1, lon1 = data[i]
+            id2, name2, line2, lat2, lon2 = data[j]
+            if name1 == name2 and line1 != line2:
+                edges.append((id1, id2, transfer_same_station))
+                edges.append((id2, id1, transfer_same_station))
+                transfer_edges_count += 1
+    
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            id1, name1, line1, lat1, lon1 = data[i]
+            id2, name2, line2, lat2, lon2 = data[j]
+            distance = manhattan_distance(lat1, lon1, lat2, lon2)
+            cost = distance * walking_speed  # Calculation of costs
+
+            if cost < max_walking_duration and not (line1 == line2 or name1 == name2):
+                edges.append((id1, id2, cost))
+                edges.append((id2, id1, cost))
+                remaining_edges_count += 1
+
+    print(f"Number of edges within the same line: {line_edges_count}")
+    print(f"Number of edges for transfers at the same station: {transfer_edges_count}")
+    print(f"Number of remaining edges: {remaining_edges_count}")
+    return edges
+
+# calculation of shortest paths
+def calculate_shortest_paths(graph, nodes):
+    paths = []
+    for start_node in nodes:
+        for end_node in nodes:
+            if start_node != end_node:
+                shortest_path = graph.dijkstra(start_node, end_node)
+                paths.append((start_node, end_node, shortest_path))
+    return paths
+
+def save_paths_to_csv(paths, filename):
+    with open(filename, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["start_station", "end_station", "connection"])
+        for start, end, path in paths:
+            writer.writerow([start, end, " -> ".join(map(str, path))])
+
+
+
+def generate_shortest_paths_graph():
+    station_data = read_csv_data("data/continuous_subway_data.csv")
+    station_data.sort(key=lambda x: x[0])
+    edges = create_edges(station_data)
+    graph = Graph(edges)
+
+    nodes = list(graph.vertices())  # List of all nodes (stations)
+    shortest_paths = calculate_shortest_paths(graph, nodes)
+    print("Saving begins")
+    save_paths_to_csv(shortest_paths, "data/shortest_paths.csv")
