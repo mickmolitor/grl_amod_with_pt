4dae9bf5e281e7aeb9d4abf6eadc0ef4de1b9ba7
Dominik Phillip Theilen <dominik.phillip.theilen@sap.com>
2024-05-01 08:13:10 +0200

    Cleanup ProgramParams

--- params/program_params.py
+++ params/program_params.py
@@ -13,62 +13,17 @@ class DataSet(Enum):
 
 class ProgramParams:
 
+    ######################################################################################################
+    ############### Execution specific ###############
+    EXECUTION_MODE = None
+
     SIMULATION_DATE = datetime(2023, 7, 10)
 
     DATA_SET = DataSet.FOR_HIRE
 
-    # If algorithm should do relocation
-    FEATURE_RELOCATION_ENABLED = True
-
-    IDLING_COST = 5
-
-    AMOUNT_OF_VEHICLES = 100
-
-    LEARNING_RATE = 0.025  # See Tang et al. (2021)
-
-    MAXIMUM_STATE_VALUE = 1000
-
-    MINIMUM_STATE_VALUE = -1000
-
-    # Time it takes for customers to enter or leave the public transport system in seconds
-    PUBLIC_TRANSPORT_ENTRY_EXIT_TIME = 120   
-
-    # Radius for relocation in meters
-    RELOCATION_RADIUS = 10000
-
-    # Pick-up distance threshold (how far away driver consider new orders) in meter
-    # Equal to 5 minutes
-    PICK_UP_DISTANCE_THRESHOLD = 1900  # im Paper 950 Meter
-
-    # Duration how long orders can be matched with drivers in seconds
-    ORDER_EXPIRY_DURATION = 120
-
-    # How much direct trips should be discounted in the optimization
-    DIRECT_TRIP_DISCOUNT_FACTOR = 0.5
 
     ######################################################################################################
-    ############### Deep Reinforcement Learning ###############
-
-    # Number of iterations until the weights of main net are copied to target net
-    MAIN_AND_TARGET_NET_SYNC_ITERATIONS = 60
-
-    # Hyperparameter that says how the online policy learning should be 
-    # influenced by offline policy learning
-    OMEGA = 0.2
-
-    ######################################################################################################
-    ############### Variable, aber wollen wir kaum verändern ###############
-
-    # Time the vehicle needs to idle until it can relocate
-    MAX_IDLING_TIME = 300
-
-    EXECUTION_MODE = None
-
-    # Minimal trip time for routes to be eligible for combined routes in seconds
-    L1 = 0
-
-    # Maximum difference between route without vehicles time and route with vehicles time in seconds
-    L2 = 1800
+    ############### Hyperparameters ###############
 
     def DISCOUNT_FACTOR(duration_in_seconds: int) -> float:
         DISCOUNT_RATE = 0.9  # See Tang et al. (2021)
@@ -79,20 +34,55 @@ class ProgramParams:
         DISCOUNT_RATE = 0.9  # See Tang et al. (2021)
         LS = 60
         return DISCOUNT_RATE ** (duration_in_seconds / LS)
-    
 
+    LEARNING_RATE = 0.025  # See Tang et al. (2021)
+
+
+    ######################################################################################################
+    ############### Operational parameters ###############
+
+    IDLING_COST = 5
+
+    AMOUNT_OF_VEHICLES = 100
+
+    # Radius for relocation in meters
+    RELOCATION_RADIUS = 10000
+
+    # How much direct trips should be discounted in the optimization
+    DIRECT_TRIP_DISCOUNT_FACTOR = 0.5
+
+
+    ######################################################################################################
+    ############### Deep Reinforcement Learning ###############
+
+    # Number of iterations until the weights of main net are copied to target net
+    MAIN_AND_TARGET_NET_SYNC_ITERATIONS = 60
+
+
+    ######################################################################################################
+    ############### Other environment values which are static ###############
+
+    # Time the vehicle needs to idle until it can relocate
+    MAX_IDLING_TIME = 300
+
+    # Minimal trip time for routes to be eligible for combined routes in seconds
+    L1 = 0
+
+    # Maximum difference between route without vehicles time and route with vehicles time in seconds
+    L2 = 1800
+    
     GRID_INTERVAL_UPDATE_RATE = 1800
     
+
     ##########################################################################################################
-    ############### Inilization of the static_data ###############
-    # Hierfür muss die Datei graph_für_OEPNV_Netz.py geändert werden
-    # zusätzlich zu den Parametern, wird WALKING_SPEED für graph_für_OEPNV_Netz.py verwendet
+    ############### Inilization of the public transport network ###############
     STATION_DURATION = 80  # Fahrzeit für eine Station
     TRANSFER_SAME_STATION = 300  # Setzen Sie hier den Wert für Umsteige_selbe_Station
     MAX_WALKING_DURATION = 600
 
+
     ######################################################################################################
-    ############### FIX ###############
+    ############### Fix environment variables ###############
     # File paths to orders
     def ORDERS_FILE_PATH() :
         return f"data/{ProgramParams.DATA_SET.value}/orders_{ProgramParams.SIMULATION_DATE.strftime('%Y-%m-%d')}.csv"
@@ -119,9 +109,22 @@ class ProgramParams:
         # Source: https://www.introducingnewyork.com/subway
         # https://www.humiliationstudies.org/documents/NYsubwaymap.pdf
     
+    # Time it takes for customers to enter or leave the public transport system in seconds
+    PUBLIC_TRANSPORT_ENTRY_EXIT_TIME = 120  
     
     # Static vehicle speed in m/s -> assume these small busses driving in Berlin
     VEHICLE_SPEED = 6.33
 
     # Static walking speed in m/s
     WALKING_SPEED = 1
+
+    # If algorithm should do relocation
+    def FEATURE_RELOCATION_ENABLED() -> bool:
+        return ProgramParams.EXECUTION_MODE == Mode.GRAPH_REINFORCEMENT_LEARNING
+    
+    # Pick-up distance threshold (how far away a vehicle consider new orders) in meter
+    # Equal to 5 minutes
+    PICK_UP_DISTANCE_THRESHOLD = 1900  # 950 meters in Feng et al. (2022)
+
+    # Duration how long orders can be matched with vehicles in seconds
+    ORDER_EXPIRY_DURATION = 120
