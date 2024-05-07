5b21dd7ed368b8d522215ebf646bec7251af1764
Dominik Phillip Theilen <dominik.phillip.theilen@sap.com>
2024-05-07 07:41:48 +0200

    Fix numerical data analysis

--- analysis/numerical_analysis.py
+++ analysis/numerical_analysis.py
@@ -26,42 +26,57 @@ def load_and_merge_data(base_path, filename, dates):
         merged_df = pd.DataFrame()
     return merged_df
 
-def numerical_analysis():
-    set_params()
+def analyse():
+    output_data = {
+        "Average time reduction per vehicle per hour in minutes": 0.0,
+        "Amount of served orders per day": 0.0,
+        "Combi route quota": 0.0,
+        "Average vehicle route length in meters": 0.0,
+        "Vehicle workload": 0.0,
+        "Relocation workload": 0.0,
+        "Idling quota": 0.0,
+        "Average time reduction per order in minutes": 0.0,
+        "Percentage of accepted orders": 0.0,
+        "Average distance of relocation in meters": 0.0
+    }
 
-    result_str = ""
     base_path = f"store/{ProgramParams.DATA_OUTPUT_FILE_PATH()}/data"
 
     dates = [datetime.date(2023, 7, 24) + datetime.timedelta(days=x) for x in range(7)]
 
-    print(dates)
-    # Laden und Zusammenführen der Daten für jeden Dateityp
     data = load_and_merge_data(base_path, "tripdata", dates)
     datarl = load_and_merge_data(base_path, "relocation_trip_data", dates)
     datadriver = load_and_merge_data(base_path, "vehicle_data", dates)
     dataorders = load_and_merge_data("data/for_hire", "orders_", dates)
 
 
-    result_str += f'Average time reduction: {round(sum(data["time_reduction"]/60/24/len(dates)/ProgramParams.AMOUNT_OF_VEHICLES), 2)}\n'
-    result_str += f'Amount of served orders per day: {len(data)/len(dates)}\n'
-    result_str += f'Combi route quota: {round(100*sum(data["combi_route"]/len(data)), 2)}%\n'
-
-    result_str += f'Average vehicle route length: {round(sum(data["total_vehicle_distance"]/len(data)),2)}\n'
+    output_data["Average time reduction per vehicle per hour in minutes"] = round(sum(data["time_reduction"]/60/24/len(dates)/ProgramParams.AMOUNT_OF_VEHICLES), 2)
+    output_data["Amount of served orders per day"] = int(len(data)/len(dates))
+    output_data["Combi route quota"] = f"{round(100*sum(data['combi_route']/len(data)), 2)}%"
+    output_data["Average vehicle route length in meters"] = round(sum(data["total_vehicle_distance"]/len(data)),2)
 
     amount_occupied = datadriver.loc[datadriver["status"] == "occupied", "status"].count()
     amount_relocation = datadriver.loc[datadriver["status"] == "relocation", "status"].count()
     amount_idling = datadriver.loc[datadriver["status"] == "idling", "status"].count()
-    result_str += f"Vehicle workload: {amount_occupied/len(datadriver)} (Relocation: {amount_relocation/len(datadriver)}, Idling: {amount_idling/len(datadriver)})\n"
-
-    result_str += f'Total amount of served orders: {len(data)}\n'
+    output_data["Vehicle workload"] = f"{round(100*amount_occupied/len(datadriver), 2)}%"
+    output_data["Relocation workload"] = f"{round(100*amount_relocation/len(datadriver), 2)}%"
+    output_data["Idling quota"] = f"{round(100*amount_idling/len(datadriver), 2)}%"
 
     total_time_reduction =  round(sum(data["time_reduction"]/60), 2)
-    result_str += f'Total time reduction in minutes: {total_time_reduction}\n'
-    result_str += f'Average time reduction per order in minutes: {total_time_reduction/len(dataorders)}\n'
-    result_str += f'Percentage of accepted orders: {round(100*len(data)/len(dataorders), 2)}%\n'
+    output_data["Average time reduction per order in minutes"] = round(total_time_reduction/len(data), 2)
+    output_data["Percentage of accepted orders"] = round(100*len(data)/len(dataorders), 2)
+    output_data["Average distance of relocation in meters"] = round(sum(datarl["distance"]/len(datarl)), 2)
 
-    result_str += f'Amount of relocations: {len(datarl)}\n'
-    result_str += f'Average distance of relocation in meters: {round(sum(datarl["distance"]/len(datarl)), 2)}\n'
+    return output_data
+
+def numerical_analysis():
+    set_params()
+
+    result_str = ""
+    
+    results = analyse()
+    for key in results:
+        result_str += f"{key}: {results[key]}\n"
 
     figure_path = f"store/{ProgramParams.DATA_OUTPUT_FILE_PATH()}/figures"
     if not os.path.exists(figure_path):
@@ -89,32 +104,9 @@ def numerical_comparison():
     for value in values:
         ProgramParams.set_member(parameter, value)
 
-        base_path = f"store/{ProgramParams.DATA_OUTPUT_FILE_PATH()}/data"
-
-        dates = [datetime.date(2023, 7, 24) + datetime.timedelta(days=x) for x in range(7)]
-
-        data = load_and_merge_data(base_path, "tripdata", dates)
-        datarl = load_and_merge_data(base_path, "relocation_trip_data", dates)
-        datadriver = load_and_merge_data(base_path, "vehicle_data", dates)
-        dataorders = load_and_merge_data("data/for_hire", "orders_", dates)
-
-
-        output_data["Average time reduction per vehicle per hour in minutes"].append(round(sum(data["time_reduction"]/60/24/len(dates)/ProgramParams.AMOUNT_OF_VEHICLES), 2))
-        output_data["Amount of served orders per day"].append(int(len(data)/len(dates)))
-        output_data["Combi route quota"].append(f"{round(100*sum(data['combi_route']/len(data)), 2)}%")
-        output_data["Average vehicle route length in meters"].append(round(sum(data["total_vehicle_distance"]/len(data)),2))
-
-        amount_occupied = datadriver.loc[datadriver["status"] == "occupied", "status"].count()
-        amount_relocation = datadriver.loc[datadriver["status"] == "relocation", "status"].count()
-        amount_idling = datadriver.loc[datadriver["status"] == "idling", "status"].count()
-        output_data["Vehicle workload"].append(f"{round(100*amount_occupied/len(datadriver), 2)}%")
-        output_data["Relocation workload"].append(f"{round(100*amount_relocation/len(datadriver), 2)}%")
-        output_data["Idling quota"].append(f"{round(100*amount_idling/len(datadriver), 2)}%")
-
-        total_time_reduction =  round(sum(data["time_reduction"]/60), 2)
-        output_data["Average time reduction per order in minutes"].append(round(total_time_reduction/len(dataorders), 2))
-        output_data["Percentage of accepted orders"].append(round(100*len(data)/len(dataorders), 2))
-        output_data["Average distance of relocation in meters"].append(round(sum(datarl["distance"]/len(datarl)), 2))
+        data = analyse()
+        for key in data:
+            output_data[key].append(data[key])
     
     figure_path = f"store/comparisons"
     if not os.path.exists(figure_path):
